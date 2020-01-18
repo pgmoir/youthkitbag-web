a6184fe61251011565f9cb194ddce0d87b66052e
Phil Moir <pgmoir@gmail.com>
2020-01-18 16:47:06 +0000

    Convert to functional component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { Link, Router, Route, Switch } from 'react-router-dom';
-import { connect } from 'react-redux';
 import PrivateRoute from '../helpers/privateRoute';
 import history from '../helpers/history';
-
 import User from './auth/User';
 import Header from './includes/Header';
 import Footer from './includes/Footer';
-
 import Home from './Home';
 import Why from './Why';
 import Pricing from './Pricing';
@@ -15,16 +12,13 @@ import Terms from './site/Terms';
 import Privacy from './site/Privacy';
 import Security from './site/Security';
 import Accessibility from './site/Accessibility';
-
 import SignUp from './auth/SignUpPage';
 import Login from './auth/LoginPage';
 import Reset from './auth/ResetPage';
 import NewPassword from './auth/NewPasswordPage';
 import Logout from './auth/Logout';
 import Token from './auth/Token';
-
 import PurchaseSubscriptionPage from './PurchaseSubscriptionPage';
-
 import Groups from './group/Groups';
 import GroupPage from './group/GroupPage';
 import GroupStatus from './group/GroupStatus';
@@ -32,158 +26,139 @@ import GroupMembers from './group/GroupMembers';
 import GroupMemberState from './group/GroupMemberState';
 import GroupMemberJoin from './group/GroupMemberJoin';
 import GroupMemberLeave from './group/GroupMemberLeave';
-
 import KitBag from './kitbag/kit/KitBag';
 import KitPage from './kitbag/kit/KitPage';
 import KitDelete from './kitbag/kit/KitDelete';
-
 import MarketKitPage from './kitbag/market/MarketKitPage';
 import MarketKitDelete from './kitbag/market/MarketKitDelete';
-
 import MarketItems from './market/MarketItems';
 import MarketItemViewPage from './market/MarketItemViewPage';
-
 import SettingsPage from './account/SettingsPage';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="react-body">
-        <Router history={history}>
-          <User />
-          <Header />
-          <main>
-            <noscript>You need to enable JavaScript to run this app.</noscript>
-            <Link
-              id="main-acc-jump"
-              className="sr-only sr-only-focusable"
-              to="#footer-acc-jump"
-            >
-              <div className="container">
-                <span className="skiplink-text">Skip to footer content</span>
-              </div>
-            </Link>
-            <Switch>
-              <Route path="/" exact component={Home} />
-              <Route path="/why" exact component={Why} />
-              <Route path="/pricing" exact component={Pricing} />
+const App = () => {
+  return (
+    <div className="react-body">
+      <Router history={history}>
+        <User />
+        <Header />
+        <main>
+          <noscript>You need to enable JavaScript to run this app.</noscript>
+          <Link
+            id="main-acc-jump"
+            className="sr-only sr-only-focusable"
+            to="#footer-acc-jump"
+          >
+            <div className="container">
+              <span className="skiplink-text">Skip to footer content</span>
+            </div>
+          </Link>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/why" exact component={Why} />
+            <Route path="/pricing" exact component={Pricing} />
 
-              <Route path="/site/terms" exact component={Terms} />
-              <Route path="/site/privacy" exact component={Privacy} />
-              <Route path="/site/security" exact component={Security} />
-              <Route
-                path="/site/accessibility"
-                exact
-                component={Accessibility}
-              />
+            <Route path="/site/terms" exact component={Terms} />
+            <Route path="/site/privacy" exact component={Privacy} />
+            <Route path="/site/security" exact component={Security} />
+            <Route path="/site/accessibility" exact component={Accessibility} />
 
-              <Route path="/auth/signup" exact component={SignUp} />
-              <Route path="/auth/login" exact component={Login} />
-              <Route path="/auth/reset" exact component={Reset} />
-              <Route path="/auth/token/:token" exact component={Token} />
-              <Route
-                path="/auth/newpassword/:key"
-                exact
-                component={NewPassword}
-              />
-              <Route path="/auth/logout" exact component={Logout} />
+            <Route path="/auth/signup" exact component={SignUp} />
+            <Route path="/auth/login" exact component={Login} />
+            <Route path="/auth/reset" exact component={Reset} />
+            <Route path="/auth/token/:token" exact component={Token} />
+            <Route
+              path="/auth/newpassword/:key"
+              exact
+              component={NewPassword}
+            />
+            <Route path="/auth/logout" exact component={Logout} />
 
-              <PrivateRoute
-                path="/purchase/subscription/:subscriptionId"
-                exact
-                component={PurchaseSubscriptionPage}
-              />
+            <PrivateRoute
+              path="/purchase/subscription/:subscriptionId"
+              exact
+              component={PurchaseSubscriptionPage}
+            />
 
-              <PrivateRoute
-                path="/market/view/:marketId"
-                exact
-                component={MarketItemViewPage}
-              />
-              <Route path="/market" component={MarketItems} />
+            <PrivateRoute
+              path="/market/view/:marketId"
+              exact
+              component={MarketItemViewPage}
+            />
+            <Route path="/market" component={MarketItems} />
 
-              <PrivateRoute
-                path="/kitbag/kit/:accountId/new"
-                component={KitPage}
-              />
-              <PrivateRoute
-                path="/kitbag/kit/:accountId/edit/:kitId"
-                exact
-                component={KitPage}
-              />
-              <PrivateRoute
-                path="/kitbag/kit/:accountId/delete/:kitId"
-                exact
-                component={KitDelete}
-              />
-              <PrivateRoute path="/kitbag/kit/:accountId" component={KitBag} />
+            <PrivateRoute
+              path="/kitbag/kit/:accountId/new"
+              component={KitPage}
+            />
+            <PrivateRoute
+              path="/kitbag/kit/:accountId/edit/:kitId"
+              exact
+              component={KitPage}
+            />
+            <PrivateRoute
+              path="/kitbag/kit/:accountId/delete/:kitId"
+              exact
+              component={KitDelete}
+            />
+            <PrivateRoute path="/kitbag/kit/:accountId" component={KitBag} />
 
-              <PrivateRoute
-                path="/kitbag/market/:accountId/new"
-                exact
-                component={MarketKitPage}
-              />
-              <PrivateRoute
-                path="/kitbag/market/:accountId/add/:kitId/:marketType"
-                exact
-                component={MarketKitPage}
-              />
-              <PrivateRoute
-                path="/kitbag/market/:accountId/edit/:marketId"
-                exact
-                component={MarketKitPage}
-              />
-              <PrivateRoute
-                path="/kitbag/market/:accountId/delete/:marketId"
-                exact
-                component={MarketKitDelete}
-              />
+            <PrivateRoute
+              path="/kitbag/market/:accountId/new"
+              exact
+              component={MarketKitPage}
+            />
+            <PrivateRoute
+              path="/kitbag/market/:accountId/add/:kitId/:marketType"
+              exact
+              component={MarketKitPage}
+            />
+            <PrivateRoute
+              path="/kitbag/market/:accountId/edit/:marketId"
+              exact
+              component={MarketKitPage}
+            />
+            <PrivateRoute
+              path="/kitbag/market/:accountId/delete/:marketId"
+              exact
+              component={MarketKitDelete}
+            />
 
-              <PrivateRoute path="/groups/new" exact component={GroupPage} />
-              <PrivateRoute
-                path="/groups/status/:groupId"
-                exact
-                component={GroupStatus}
-              />
-              <PrivateRoute
-                path="/groups/:groupId/members/:memberId/:state"
-                exact
-                component={GroupMemberState}
-              />
-              <PrivateRoute
-                path="/groups/:groupId/members"
-                exact
-                component={GroupMembers}
-              />
-              <PrivateRoute
-                path="/groups/:groupId/join"
-                exact
-                component={GroupMemberJoin}
-              />
-              <PrivateRoute
-                path="/groups/:groupId/leave"
-                exact
-                component={GroupMemberLeave}
-              />
-              <PrivateRoute
-                path="/groups/:groupId"
-                exact
-                component={GroupPage}
-              />
-              <PrivateRoute path="/groups" component={Groups} />
+            <PrivateRoute path="/groups/new" exact component={GroupPage} />
+            <PrivateRoute
+              path="/groups/status/:groupId"
+              exact
+              component={GroupStatus}
+            />
+            <PrivateRoute
+              path="/groups/:groupId/members/:memberId/:state"
+              exact
+              component={GroupMemberState}
+            />
+            <PrivateRoute
+              path="/groups/:groupId/members"
+              exact
+              component={GroupMembers}
+            />
+            <PrivateRoute
+              path="/groups/:groupId/join"
+              exact
+              component={GroupMemberJoin}
+            />
+            <PrivateRoute
+              path="/groups/:groupId/leave"
+              exact
+              component={GroupMemberLeave}
+            />
+            <PrivateRoute path="/groups/:groupId" exact component={GroupPage} />
+            <PrivateRoute path="/groups" component={Groups} />
 
-              <PrivateRoute
-                path="/settings/:setting"
-                component={SettingsPage}
-              />
-            </Switch>
-          </main>
-          <Footer />
-        </Router>
-      </div>
-    );
-  }
-}
+            <PrivateRoute path="/settings/:setting" component={SettingsPage} />
+          </Switch>
+        </main>
+        <Footer />
+      </Router>
+    </div>
+  );
+};
 
-const mapStateToProps = state => ({ auth: state.auth });
-
-export default connect(mapStateToProps)(App);
+export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,97 +2,95 @@ import React from 'react';
 import Alert from './includes/Alert';
 import SignUpForm from './auth/SignUpForm';
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div>
-        <section
-          id="main"
-          className="container-fluid"
-          aria-label="main body of content plus related links and features"
-        >
-          <div className="jumbotron">
-            <div className="container text-center">
-              <div className="row">
-                <div className="col-12 col-lg-6">
-                  <h1>YouthKitbag</h1>
-                  <h2 className="pb-3">Inventory, Trade, Report</h2>
-                  <h3 className="">school kit, club kit, team kit, any kit</h3>
-                  <h4 className="pb-5">
-                    sports gear, musical instruments, school uniforms, more ...
-                  </h4>
-                </div>
-                <div className="col-12 col-lg-6">
-                  <div className="row">
-                    <div className="col-12 col-lg-10 mb-3 mx-auto bg-dark text-white p-5 rounded-lg">
-                      <Alert />
-                      <SignUpForm />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="container mb-3">
+const Home = () => {
+  return (
+    <div>
+      <section
+        id="main"
+        className="container-fluid"
+        aria-label="main body of content plus related links and features"
+      >
+        <div className="jumbotron">
+          <div className="container text-center">
             <div className="row">
-              <div className="col-12 col-sm-6 col-md-3">
-                <article className="card card-link card-b1">
-                  <div className="d-flex">
-                    <div className="blank-square bg-light" />
-                  </div>
-                  <div className="card-body">
-                    <h3 className="card-title h6 ellipsis bg-light hgt-2">
-                      &nbsp;
-                    </h3>
-                    <p className="card-text ellipsis bg-light hgt-3">&nbsp;</p>
-                  </div>
-                </article>
+              <div className="col-12 col-lg-6">
+                <h1>YouthKitbag</h1>
+                <h2 className="pb-3">Inventory, Trade, Report</h2>
+                <h3 className="">school kit, club kit, team kit, any kit</h3>
+                <h4 className="pb-5">
+                  sports gear, musical instruments, school uniforms, more ...
+                </h4>
               </div>
-              <div className="col-12 col-sm-6 col-md-3">
-                <article className="card card-link card-b1">
-                  <div className="d-flex">
-                    <div className="blank-square bg-light" />
+              <div className="col-12 col-lg-6">
+                <div className="row">
+                  <div className="col-12 col-lg-10 mb-3 mx-auto bg-dark text-white p-5 rounded-lg">
+                    <Alert />
+                    <SignUpForm />
                   </div>
-                  <div className="card-body">
-                    <h3 className="card-title h6 ellipsis bg-light hgt-2">
-                      &nbsp;
-                    </h3>
-                    <p className="card-text ellipsis bg-light hgt-3">&nbsp;</p>
-                  </div>
-                </article>
-              </div>
-              <div className="col-12 col-sm-6 col-md-3">
-                <article className="card card-link card-b1">
-                  <div className="d-flex">
-                    <div className="blank-square bg-light" />
-                  </div>
-                  <div className="card-body">
-                    <h3 className="card-title h6 ellipsis bg-light hgt-2">
-                      &nbsp;
-                    </h3>
-                    <p className="card-text ellipsis bg-light hgt-3">&nbsp;</p>
-                  </div>
-                </article>
-              </div>
-              <div className="col-12 col-sm-6 col-md-3">
-                <article className="card card-link card-b1">
-                  <div className="d-flex">
-                    <div className="blank-square bg-light" />
-                  </div>
-                  <div className="card-body">
-                    <h3 className="card-title h6 ellipsis bg-light hgt-2">
-                      &nbsp;
-                    </h3>
-                    <p className="card-text ellipsis bg-light hgt-3">&nbsp;</p>
-                  </div>
-                </article>
+                </div>
               </div>
             </div>
           </div>
-        </section>
-      </div>
-    );
-  }
-}
+        </div>
+        <div className="container mb-3">
+          <div className="row">
+            <div className="col-12 col-sm-6 col-md-3">
+              <article className="card card-link card-b1">
+                <div className="d-flex">
+                  <div className="blank-square bg-light" />
+                </div>
+                <div className="card-body">
+                  <h3 className="card-title h6 ellipsis bg-light hgt-2">
+                    &nbsp;
+                  </h3>
+                  <p className="card-text ellipsis bg-light hgt-3">&nbsp;</p>
+                </div>
+              </article>
+            </div>
+            <div className="col-12 col-sm-6 col-md-3">
+              <article className="card card-link card-b1">
+                <div className="d-flex">
+                  <div className="blank-square bg-light" />
+                </div>
+                <div className="card-body">
+                  <h3 className="card-title h6 ellipsis bg-light hgt-2">
+                    &nbsp;
+                  </h3>
+                  <p className="card-text ellipsis bg-light hgt-3">&nbsp;</p>
+                </div>
+              </article>
+            </div>
+            <div className="col-12 col-sm-6 col-md-3">
+              <article className="card card-link card-b1">
+                <div className="d-flex">
+                  <div className="blank-square bg-light" />
+                </div>
+                <div className="card-body">
+                  <h3 className="card-title h6 ellipsis bg-light hgt-2">
+                    &nbsp;
+                  </h3>
+                  <p className="card-text ellipsis bg-light hgt-3">&nbsp;</p>
+                </div>
+              </article>
+            </div>
+            <div className="col-12 col-sm-6 col-md-3">
+              <article className="card card-link card-b1">
+                <div className="d-flex">
+                  <div className="blank-square bg-light" />
+                </div>
+                <div className="card-body">
+                  <h3 className="card-title h6 ellipsis bg-light hgt-2">
+                    &nbsp;
+                  </h3>
+                  <p className="card-text ellipsis bg-light hgt-3">&nbsp;</p>
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
 
 export default Home;
