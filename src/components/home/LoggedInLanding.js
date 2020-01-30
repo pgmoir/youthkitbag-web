6f594a4f250b07aeb2b617e14c07294abd58fc29
Phil Moir <pgmoir@gmail.com>
2020-01-30 13:02:38 +0000

    Add loreum ipsum text to home page blocks

--- src/components/home/LoggedInLanding.js
+++ src/components/home/LoggedInLanding.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import Alert from '../includes/Alert';
 import Title from '../includes/title/Title';
+import { connect } from 'react-redux';
 
-const LoggedInLanding = () => {
+const mapStateToProps = state => ({
+  user: state.user
+});
+
+const LoggedInLanding = ({ user }) => {
+  function getDisplayName() {
+    if (
+      !user ||
+      !user.profile ||
+      (!user.profile.username && !user.profile.firstname)
+    )
+      return '';
+    return user.profile.username
+      ? user.profile.username
+      : user.profile.firstname;
+  }
+
   return (
     <div>
       <section
@@ -10,13 +27,87 @@ const LoggedInLanding = () => {
         className="container-fluid"
         aria-label="main body of content plus related links and features"
       >
-        <Title title="Personal Settings" />
+        <Title title={`Welcome ${getDisplayName()} to YouthKitbag`} />
         <div className="container">
           <Alert />
+          <div className="row">
+            <div className="col-12 col-md-6">
+              <h3>Profile to update</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+                et sodales mauris. Cras semper blandit nisl, quis lacinia felis
+                cursus nec. Nulla libero ipsum, viverra vitae mauris vel,
+                porttitor pellentesque Nullam a scelerisque urna. Mauris risus
+                lectus, egestas sit amet diam sit amet, porttitor elementum
+                neque. Phasellus volutpat nisi et elit venenatis ultricies. Sed
+                vel molestie metus.
+              </p>
+              <p>
+                Fusce iaculis lorem in ornare rhoncus. Nunc sed ullamcorper mi,
+                at euismod eros. Pellentesque tristique hendrerit dapibus. Sed
+                ac risus id metus augue. Sed viverra mollis luctus. Nullam
+                aliquet turpis a velit lobortis cursus.
+              </p>
+            </div>
+            <div className="col-12 col-md-6">
+              <h3>New market items</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+                et sodales mauris. Cras semper blandit nisl, quis lacinia felis
+                cursus nec. Nulla libero ipsum, viverra vitae mauris vel,
+                porttitor pellentesque Nullam a scelerisque urna. Mauris risus
+                lectus, egestas sit amet diam sit amet, porttitor elementum
+                neque. Phasellus volutpat nisi et elit venenatis ultricies. Sed
+                vel molestie metus.
+              </p>
+              <p>
+                Fusce iaculis lorem in ornare rhoncus. Nunc sed ullamcorper mi,
+                at euismod eros. Pellentesque tristique hendrerit dapibus. Sed
+                ac risus id metus augue. Sed viverra mollis luctus. Nullam
+                aliquet turpis a velit lobortis cursus.
+              </p>
+            </div>
+            <div className="col-12 col-md-6">
+              <h3>New kit items</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+                et sodales mauris. Cras semper blandit nisl, quis lacinia felis
+                cursus nec. Nulla libero ipsum, viverra vitae mauris vel,
+                porttitor pellentesque Nullam a scelerisque urna. Mauris risus
+                lectus, egestas sit amet diam sit amet, porttitor elementum
+                neque. Phasellus volutpat nisi et elit venenatis ultricies. Sed
+                vel molestie metus.
+              </p>
+              <p>
+                Fusce iaculis lorem in ornare rhoncus. Nunc sed ullamcorper mi,
+                at euismod eros. Pellentesque tristique hendrerit dapibus. Sed
+                ac risus id metus augue. Sed viverra mollis luctus. Nullam
+                aliquet turpis a velit lobortis cursus.
+              </p>
+            </div>
+            <div className="col-12 col-md-6">
+              <h3>Group Approvals</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+                et sodales mauris. Cras semper blandit nisl, quis lacinia felis
+                cursus nec. Nulla libero ipsum, viverra vitae mauris vel,
+                porttitor pellentesque Nullam a scelerisque urna. Mauris risus
+                lectus, egestas sit amet diam sit amet, porttitor elementum
+                neque. Phasellus volutpat nisi et elit venenatis ultricies. Sed
+                vel molestie metus.
+              </p>
+              <p>
+                Fusce iaculis lorem in ornare rhoncus. Nunc sed ullamcorper mi,
+                at euismod eros. Pellentesque tristique hendrerit dapibus. Sed
+                ac risus id metus augue. Sed viverra mollis luctus. Nullam
+                aliquet turpis a velit lobortis cursus.
+              </p>
+            </div>
+          </div>
         </div>
       </section>
     </div>
   );
 };
 
-export default LoggedInLanding;
+export default connect(mapStateToProps, null)(LoggedInLanding);
