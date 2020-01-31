8562538a6937dae4543978edc9d858656150d0ee
Phil Moir <pgmoir@gmail.com>
2020-01-31 16:42:32 +0000

    Update reset form to use effects and standard structure

--- src/components/auth/ResetForm.js
+++ src/components/auth/ResetForm.js
@@ -1,47 +1,59 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 import useForm from '../hooks/useForm';
 import { reset } from '../../actions/AuthActions';
 import validate from './ResetFormValidationRules';
-import { useDispatch } from 'react-redux';
+import { TextForm } from '../includes/forms';
 
-const ResetForm = () => {
-  const dispatch = useDispatch();
+const mapDispatchToProps = {
+  reset
+};
 
+const mapStateToProps = state => ({
+  newErrors: state.toast.errors
+});
+
+const ResetForm = ({ reset, newErrors }) => {
   const initialValues = {
     email: ''
   };
 
-  const { values, handleChange, handleSubmit, errors } = useForm(
+  const { values, handleChange, handleSubmit, errors, setErrors } = useForm(
     initialValues,
     resetSubmit,
     validate
   );
 
+  useEffect(() => {
+    if (newErrors) {
+      setErrors(newErrors);
+    }
+  }, [newErrors, setErrors]);
+
   function resetSubmit() {
-    dispatch(reset(values.email));
+    reset(values.email);
   }
 
   return (
     <form className="w-100 d-block" onSubmit={handleSubmit}>
-      <div className="form-group">
-        <label htmlFor="email">Email</label>
-        <input
-          className={`form-control ${errors.email && 'is-invalid'}`}
-          name="email"
-          type="email"
-          onChange={handleChange}
-          value={values.email}
-          aria-describedby="email"
-          autoComplete="username email"
-          required
-        />
-        {errors.email && <div className="invalid-feedback">{errors.email}</div>}
-      </div>
-      <button className="btn btn-primary" type="submit">
+      <TextForm
+        colFormat="3-9"
+        type="email"
+        label="Email"
+        value={values.email}
+        field="email"
+        handleChange={handleChange}
+        error={errors.email}
+        autoComplete="username email"
+      />
+      <button
+        className="d-block btn btn-success btn-lg py-3 px-5 mt-3 mx-auto"
+        type="submit"
+      >
         Reset Password
       </button>
     </form>
   );
 };
 
-export default ResetForm;
+export default connect(mapStateToProps, mapDispatchToProps)(ResetForm);
--- src/components/auth/ResetPage.js
+++ src/components/auth/ResetPage.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
 import Title from '../includes/title/Title';
 import Alert from '../includes/Alert';
 import ResetForm from './ResetForm';
+import { Link } from 'react-router-dom';
 
 const ResetPage = () => {
   return (
@@ -14,6 +14,13 @@ const ResetPage = () => {
         aria-label="main body of content plus related links and features"
       >
         <div className="container">
+          <p className="lead">
+            If you have forgotten your password, then enter your email here and
+            you will be sent a link to create a new password. Please be aware
+            that this link is time limited, and will expire in an hour after
+            sending. If actually you know you&apos;re password,{' '}
+            <Link to="/auth/login">login direct to your account</Link>.
+          </p>
           <div className="row">
             <div className="col-12 col-md-6 mb-3 mx-auto">
               <Alert />
