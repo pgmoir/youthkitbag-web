3b744b8e64e9e137a30ae0deb3e8c3d15b935b68
Phil Moir <pgmoir@gmail.com>
2020-04-01 22:39:11 +0100

    #56 authenticating user message not displaying within container

--- src/components/auth/Token.js
+++ src/components/auth/Token.js
@@ -13,7 +13,19 @@ const Token = ({ authenticateToken, match }) => {
     authenticateToken(token);
   }, [token, authenticateToken]);
 
-  return <h1 className="loading-text">Authenticating user...</h1>;
+  return (
+    <section
+      id="title"
+      className="container-fluid px-0"
+      role="banner"
+      aria-label="breadcrumb navigation and page title"
+    >
+      <div className="container">
+        <div className="d-block hgt-1"></div>
+        <h1 className="h-standard pb-2">Authenticating user ...</h1>
+      </div>
+    </section>
+  );
 };
 
 export default connect(null, mapDispatchToProps)(Token);
