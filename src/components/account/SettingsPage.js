71624d89eebb51616ab82c26d37ed6ea6a58dbbf
Phil Moir <pgmoir@gmail.com>
2020-04-01 19:43:11 +0100

    #54 Selected settings page should highlight tab on mobile

--- src/components/account/SettingsPage.js
+++ src/components/account/SettingsPage.js
@@ -6,12 +6,12 @@ import ProfileForm from './ProfileForm';
 import AccountsPage from './AccountsPage';
 import GroupsPage from './GroupsPage';
 import PackagePage from './PackagePage';
-// import BadgesPage from './BadgesPage';
 import Alert from '../includes/Alert';
 import ProfileHelp from './ProfileHelp';
 import AccountsHelp from './AccountsHelp';
 import GroupsHelp from './GroupsHelp';
 import ConfigurationPage from './ConfigurationPage';
+import SettingsNav from './SettingsNav';
 
 const mapStateToProps = state => ({
   profile: state.user.profile
@@ -73,96 +73,25 @@ const SettingsPage = ({ profile }) => {
     );
   };
 
-  // const Badges = () => {
-  //   setPage('badges');
-  //   return (
-  //     <div className="container">
-  //       <h2>Badges</h2>
-  //       <BadgesPage />
-  //     </div>
-  //   );
-  // };
-
   return (
     <div>
       <Title title="Personal Settings" />
       <div className="container">
         <Alert />
         <div className="row">
-          <div className="d-block d-sm-none col-12 pb-3">
-            <div className="d-flex w-100 px-1">
-              <div className="p-2 flex-fill bg-light">
-                <Link to="/settings/profile">Profile</Link>
-              </div>
-              <div className="p-2 flex-fill bg-light">
-                <Link to="/settings/accounts">Accounts</Link>
-              </div>
-              <div className="p-2 flex-fill bg-light">
-                <Link to="/settings/groups">Groups</Link>
-              </div>
-              <div className="p-2 flex-fill bg-light">
-                <Link to="/settings/package">Package</Link>
-              </div>
-              {/* <div className="p-2 flex-fill bg-light">
-                <Link to="/settings/badges">Badges</Link>
-              </div> */}
-              <div className="p-2 flex-fill bg-light">
-                <Link to="/settings/configuration">Configuration</Link>
-              </div>
+          <div className="col-12 col-md-2 mb-3">
+            <div className="d-flex d-md-none list-group list-group-horizontal">
+              <SettingsNav page={page} fill="flex-fill" />
+            </div>
+            <div className="d-none d-md-flex list-group">
+              <SettingsNav page={page} />
             </div>
           </div>
-          <div className="d-none d-sm-block col-sm-2 pb-3">
-            <ul className="list-group">
-              <li
-                className={`list-group-item ${
-                  page === 'profile' ? 'active' : ''
-                }`}
-              >
-                <Link to="/settings/profile">Profile</Link>
-              </li>
-              <li
-                className={`list-group-item ${
-                  page === 'accounts' ? 'active' : ''
-                }`}
-              >
-                <Link to="/settings/accounts">Accounts</Link>
-              </li>
-              <li
-                className={`list-group-item ${
-                  page === 'groups' ? 'active' : ''
-                }`}
-              >
-                <Link to="/settings/groups">Groups</Link>
-              </li>
-              <li
-                className={`list-group-item ${
-                  page === 'package' ? 'active' : ''
-                }`}
-              >
-                <Link to="/settings/package">Package</Link>
-              </li>
-              {/* <li
-                className={`list-group-item ${
-                  page === 'badges' ? 'active' : ''
-                }`}
-              >
-                <Link to="/settings/badges">Badges</Link>
-              </li> */}
-              <li
-                className={`list-group-item ${
-                  page === 'configuration' ? 'active' : ''
-                }`}
-              >
-                <Link to="/settings/configuration">Configuration</Link>
-              </li>
-            </ul>
-          </div>
-          <div className="col-12 col-sm-10">
+          <div className="col-12 col-md-10">
             <Route path="/settings/profile" component={Profile} />
             <Route path="/settings/accounts" component={Accounts} />
             <Route path="/settings/groups" component={Groups} />
             <Route path="/settings/package" component={Package} />
-            {/* <Route path="/settings/badges" component={Badges} /> */}
             <Route path="/settings/configuration" component={Configuration} />
           </div>
         </div>
