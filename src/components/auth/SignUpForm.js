c5e2c758fd9c5fdd904a02a6c8bb11b86b65d593
Phil Moir <pgmoir@gmail.com>
2021-01-24 21:46:25 +0000

    refactor: signup form

--- src/components/auth/SignUpForm.js
+++ src/components/auth/SignUpForm.js
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react';
 import useForm from '../hooks/useForm';
 import { signup } from '../../actions/AuthActions';
 import { connect } from 'react-redux';
-import { TextForm } from '../includes/forms';
 import validate from '../includes/FormEmptyValidationRules';
+import TextInputStd from '../includes/controls/TextInputStd';
 
 const mapStateToProps = (state) => ({
   newErrors: state.toast.errors,
@@ -42,74 +42,76 @@ const SignUpForm = ({ newErrors, signup }) => {
   }
 
   return (
-    <form className="w-100 d-block" onSubmit={handleSubmit}>
-      <TextForm
-        colFormat="3-9"
-        label="First Name"
-        value={values.firstName}
-        field="firstName"
-        handleChange={handleChange}
-        error={errors.firstName}
-      />
-      <TextForm
-        colFormat="3-9"
-        label="Last Name"
-        value={values.lastName}
-        field="lastName"
-        handleChange={handleChange}
-        error={errors.lastName}
-      />
-      <TextForm
-        colFormat="3-9"
-        label="User Name"
-        value={values.userName}
-        field="userName"
-        handleChange={handleChange}
-        error={errors.userName}
-        autoComplete="userName"
-      />
-      <TextForm
-        colFormat="3-9"
-        label="Email"
-        type="email"
-        value={values.email}
-        field="email"
-        handleChange={handleChange}
-        error={errors.email}
-      />
-      <TextForm
-        colFormat="3-9"
-        label="Password"
-        type="password"
-        value={values.password}
-        field="password"
-        handleChange={handleChange}
-        error={errors.password}
-        autoComplete="current-password"
-      />
-      <TextForm
-        colFormat="3-9"
-        label="Confirm"
-        type="password"
-        value={values.confirmPassword}
-        field="confirmPassword"
-        handleChange={handleChange}
-        error={errors.confirmPassword}
-        autoComplete="new-password"
-      />
-      <button
-        className="d-block btn btn-success btn-lg py-3 px-5 mt-3 mx-auto"
-        type="submit"
-      >
-        Sign Up for YouthKitbag
-      </button>
-      <p className="h6 pt-3 text-center">
+    <div className="notification has-background-primary-light box">
+      <form onSubmit={handleSubmit}>
+        <TextInputStd
+          value={values.firstName}
+          field="firstName"
+          handleChange={handleChange}
+          error={errors.firstName}
+          placeHolder="Enter your First Name"
+          iconLeft="fas fa-user"
+        />
+        <TextInputStd
+          value={values.lastName}
+          field="lastName"
+          handleChange={handleChange}
+          error={errors.lastName}
+          placeHolder="Enter your Last Name"
+          iconLeft="fas fa-user-plus"
+        />
+        <TextInputStd
+          value={values.userName}
+          field="userName"
+          handleChange={handleChange}
+          error={errors.userName}
+          autoComplete="userName"
+          placeHolder="User NameSpecify a unique User Name"
+          iconLeft="fas fa-user-circle"
+        />
+        <TextInputStd
+          type="email"
+          value={values.email}
+          field="email"
+          handleChange={handleChange}
+          error={errors.email}
+          placeHolder="Enter your Email Address"
+          iconLeft="fas fa-envelope"
+        />
+        <TextInputStd
+          type="password"
+          value={values.password}
+          field="password"
+          handleChange={handleChange}
+          error={errors.password}
+          autoComplete="current-password"
+          placeHolder="Create your password"
+          iconLeft="fas fa-lock"
+        />
+        <TextInputStd
+          type="password"
+          value={values.confirmPassword}
+          field="confirmPassword"
+          handleChange={handleChange}
+          error={errors.confirmPassword}
+          autoComplete="new-password"
+          placeHolder="Confirm your password"
+          iconLeft="fas fa-passport"
+        />
+        <button
+          className="button is-large is-fullwidth is-primary"
+          type="submit"
+        >
+          Sign Up for YouthKitbag
+        </button>
+        {/* <p className="h6 pt-3 text-center">
         Alternatively use <a href={`${baseUrl}/auth/google`}>Google</a>,{' '}
         <a href={`${baseUrl}/auth/facebook`}>Facebook</a>
         {' or '}
         <a href={`${baseUrl}/auth/github`}>GitHub</a>
-      </p>
-    </form>
+      </p> */}
+      </form>
+    </div>
   );
 };
 
--- src/components/home/LoggedOutLanding.js
+++ src/components/home/LoggedOutLanding.js
@@ -5,130 +5,140 @@ import { Link } from 'react-router-dom';
 
 const LoggedOutLanding = () => {
   return (
-    <section
-      id="main"
-      className="section container is-fluid"
-      aria-label="main body of content plus related links and features"
-    >
-      <div className="jumbotron">
-        <div className="container text-center">
-          <div className="row">
-            <div className="col-12 col-lg-6">
-              <h1>YouthKitbag</h1>
-              <h2 className="pb-3">Inventory, Trade, Report</h2>
-              <h3>school kit, club kit, team kit, any kit</h3>
-              <h4 className="pb-3">
-                sports gear, musical instruments, school uniforms, more ...
-              </h4>
-              <h5 className="pb-3">
-                Why did I create this application?{' '}
-                <Link to="/why#created">I explain here</Link>
-              </h5>
-            </div>
-            <div className="col-12 col-lg-6">
-              <div className="row">
-                <div className="col-12 col-lg-10 mb-3 mx-auto bg-dark text-white p-5 rounded-lg">
-                  <Alert />
-                  <SignUpForm />
-                </div>
-              </div>
-            </div>
-          </div>
+    <div className="">
+      <div className="columns is-mobile is-centered">
+        <div className="column is-half">
+          <SignUpForm />
         </div>
       </div>
-      <div className="container mb-3">
-        <div className="row">
-          <div className="col-12 col-sm-6 col-md-3">
-            <article className="card card-link card-b1">
-              <span className="badge badge-pill badge-dark badge-fullsize badge-top-right">
-                10
-              </span>
-              <Link to="/auth/signup">
-                <img
-                  className="card-img-top"
-                  src="/images/homepage-innertubes.jpg"
-                  alt="Example of innertubes in kitbag"
-                  role="presentation"
-                />
-                <div className="card-body">
-                  <h2 className="card-title">Inventory</h2>
-                  <p className="card-text">
-                    Create multiple kitbags to keep a record of what sports,
-                    musical, hobby or school equipment you own. Keep track of
-                    expenditure, location and flag when replacement levels low.
-                  </p>
-                </div>
-              </Link>
-            </article>
-          </div>
-          <div className="col-12 col-sm-6 col-md-3">
-            <article className="card card-link card-b1">
-              <Link to="/auth/signup">
-                <img
-                  className="card-img-top"
-                  src="/images/homepage-family.jpg"
-                  alt="Example of innertubes in kitbag"
-                  role="presentation"
-                />
-                <div className="card-body">
-                  <h2 className="card-title">Family Kitbag</h2>
-                  <p className="card-text">
-                    Set up family or group kitbag, so that multiple members can
-                    access view and store equipment details. Share with family
-                    members. Help the kids find and manage their own kit!
-                  </p>
-                </div>
-              </Link>
-            </article>
-          </div>{' '}
-          <div className="col-12 col-sm-6 col-md-3">
-            <article className="card card-link card-b1">
-              <Link to="/auth/signup">
-                <img
-                  className="card-img-top"
-                  src="/images/homepage-club.jpg"
-                  alt="Example of innertubes in kitbag"
-                  role="presentation"
-                />
-                <div className="card-body">
-                  <h2 className="card-title">Clubs &amp; Teams</h2>
-                  <p className="card-text">
-                    Been scammed on eBay? Theft targetted on Facebook? There is
-                    no open trading&#185; with the general public on
-                    YouthKitbag. Only through vetted organisations that control
-                    their own membership.
-                  </p>
-                </div>
-              </Link>
-            </article>
-          </div>{' '}
-          <div className="col-12 col-sm-6 col-md-3">
-            <article className="card card-link card-b1">
-              <span className="badge badge-pill badge-dark badge-fullsize badge-top-right">
-                £3.99
-              </span>
-              <Link to="/auth/signup">
-                <img
-                  className="card-img-top"
-                  src="/images/homepage-trade.jpg"
-                  alt="Example of innertubes in kitbag"
-                  role="presentation"
-                />
-                <div className="card-body">
-                  <h2 className="card-title">Trade &amp; Report</h2>
-                  <p className="card-text">
-                    Sell or recycle your kit? Something stolen? Want new or
-                    replacement item? YouthKitbag lets you trade, request and
-                    report directly with other club, team members.
-                  </p>
-                </div>
-              </Link>
-            </article>
-          </div>
-        </div>
-      </div>
-    </section>
+    </div>
   );
+  // return (
+  //   <section
+  //     id="main"
+  //     className="section container is-fluid"
+  //     aria-label="main body of content plus related links and features"
+  //   >
+  //     <div className="jumbotron">
+  //       <div className="container text-center">
+  //         <div className="row">
+  //           <div className="col-12 col-lg-6">
+  //             <h1>YouthKitbag</h1>
+  //             <h2 className="pb-3">Inventory, Trade, Report</h2>
+  //             <h3>school kit, club kit, team kit, any kit</h3>
+  //             <h4 className="pb-3">
+  //               sports gear, musical instruments, school uniforms, more ...
+  //             </h4>
+  //             <h5 className="pb-3">
+  //               Why did I create this application?{' '}
+  //               <Link to="/why#created">I explain here</Link>
+  //             </h5>
+  //           </div>
+  //           <div className="col-12 col-lg-6">
+  //             <div className="row">
+  //               <div className="col-12 col-lg-10 mb-3 mx-auto bg-dark text-white p-5 rounded-lg">
+  //                 <Alert />
+  //                 <SignUpForm />
+  //               </div>
+  //             </div>
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </div>
+
+  //     <div className="container mb-3">
+  //       <div className="row">
+  //         <div className="col-12 col-sm-6 col-md-3">
+  //           <article className="card card-link card-b1">
+  //             <span className="badge badge-pill badge-dark badge-fullsize badge-top-right">
+  //               10
+  //             </span>
+  //             <Link to="/auth/signup">
+  //               <img
+  //                 className="card-img-top"
+  //                 src="/images/homepage-innertubes.jpg"
+  //                 alt="Example of innertubes in kitbag"
+  //                 role="presentation"
+  //               />
+  //               <div className="card-body">
+  //                 <h2 className="card-title">Inventory</h2>
+  //                 <p className="card-text">
+  //                   Create multiple kitbags to keep a record of what sports,
+  //                   musical, hobby or school equipment you own. Keep track of
+  //                   expenditure, location and flag when replacement levels low.
+  //                 </p>
+  //               </div>
+  //             </Link>
+  //           </article>
+  //         </div>
+  //         <div className="col-12 col-sm-6 col-md-3">
+  //           <article className="card card-link card-b1">
+  //             <Link to="/auth/signup">
+  //               <img
+  //                 className="card-img-top"
+  //                 src="/images/homepage-family.jpg"
+  //                 alt="Example of innertubes in kitbag"
+  //                 role="presentation"
+  //               />
+  //               <div className="card-body">
+  //                 <h2 className="card-title">Family Kitbag</h2>
+  //                 <p className="card-text">
+  //                   Set up family or group kitbag, so that multiple members can
+  //                   access view and store equipment details. Share with family
+  //                   members. Help the kids find and manage their own kit!
+  //                 </p>
+  //               </div>
+  //             </Link>
+  //           </article>
+  //         </div>{' '}
+  //         <div className="col-12 col-sm-6 col-md-3">
+  //           <article className="card card-link card-b1">
+  //             <Link to="/auth/signup">
+  //               <img
+  //                 className="card-img-top"
+  //                 src="/images/homepage-club.jpg"
+  //                 alt="Example of innertubes in kitbag"
+  //                 role="presentation"
+  //               />
+  //               <div className="card-body">
+  //                 <h2 className="card-title">Clubs &amp; Teams</h2>
+  //                 <p className="card-text">
+  //                   Been scammed on eBay? Theft targetted on Facebook? There is
+  //                   no open trading&#185; with the general public on
+  //                   YouthKitbag. Only through vetted organisations that control
+  //                   their own membership.
+  //                 </p>
+  //               </div>
+  //             </Link>
+  //           </article>
+  //         </div>{' '}
+  //         <div className="col-12 col-sm-6 col-md-3">
+  //           <article className="card card-link card-b1">
+  //             <span className="badge badge-pill badge-dark badge-fullsize badge-top-right">
+  //               £3.99
+  //             </span>
+  //             <Link to="/auth/signup">
+  //               <img
+  //                 className="card-img-top"
+  //                 src="/images/homepage-trade.jpg"
+  //                 alt="Example of innertubes in kitbag"
+  //                 role="presentation"
+  //               />
+  //               <div className="card-body">
+  //                 <h2 className="card-title">Trade &amp; Report</h2>
+  //                 <p className="card-text">
+  //                   Sell or recycle your kit? Something stolen? Want new or
+  //                   replacement item? YouthKitbag lets you trade, request and
+  //                   report directly with other club, team members.
+  //                 </p>
+  //               </div>
+  //             </Link>
+  //           </article>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </section>
+  // );
 };
 
 export default LoggedOutLanding;
