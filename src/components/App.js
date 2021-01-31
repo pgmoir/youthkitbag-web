c5b671c12c331d85f78919652dcbd71dd8d67d65
Phil Moir <pgmoir@gmail.com>
2021-01-31 22:34:38 +0000

    refactor: remove link

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Router, Route, Switch, Redirect } from 'react-router-dom';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import PrivateRoute from '../utils/privateRoute';
 import history from '../utils/history';
